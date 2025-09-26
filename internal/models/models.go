87e21f77688b3b90b3f97be0b402edcb8ed01a32
vav1 <vav1@vgserv.ru>
2025-09-26 11:51:23 +0500

    process

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,28 +1,44 @@
 package models
 
-type User struct {
-	ID    int    `json:"id"`
-	Login string `json:"login"`
-	Name  string `json:"name"`
-	Pass  string `json:"pass"`
+import "time"
+
+type DataType string
+
+const (
+	LoginPassword DataType = "login_password"
+	TextData      DataType = "text_data"
+	BinaryData    DataType = "binary_data"
+	CardData      DataType = "card_data"
+)
+
+type SecretItemData struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	Type      DataType  `json:"type"`
+	Name      string    `json:"name"`
+	Metdata   string    `json:"metdata"`
+	Data      []byte    `json:"data"`
+	Version   int       `json:"version"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
-type LoginPassData struct {
+type User struct {
+	ID           int       `json:"id"`
+	Login        string    `json:"login"`
+	PasswordHash string    `json:"password_hash"`
+	CreateAt     time.Time `json:"create_at"`
+}
+
+type LoginPasswordData struct {
 	Login    string `json:"login"`
 	Pass     string `json:"pass"`
 	Resource string `json:"resource"`
-}
-
-type Memo struct {
-	TextData string `json:"text_data"`
-}
-
-type BinaryData struct {
-	BinaryData []byte `json:"binary_data"`
 }
 
 type BankData struct {
 	CardNumber string `json:"card_number"`
 	ExpiryDate string `json:"expiry_date"`
 	CVV        string `json:"cvv"`
+	Cardholder string `json:"cardholder"`
 }
--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -24,9 +24,9 @@ func NewPostgres(dsn string) (*Postgres, error) {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
 
-	//if err := createTables(db); err != nil {
-	//	return nil, fmt.Errorf("failed to create tables: %w", err)
-	//}
+	if err := createTables(db); err != nil {
+		return nil, fmt.Errorf("failed to create tables: %w", err)
+	}
 	return &Postgres{
 		db:    db,
 		dbURL: dsn,
@@ -35,4 +35,35 @@ func NewPostgres(dsn string) (*Postgres, error) {
 
 func (p *Postgres) Test() string {
 	return "Test"
+}
+
+func createTables(db *sql.DB) error {
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            login VARCHAR(255) NOT NULL UNIQUE,
+            password_hash VARCHAR(255) NOT NULL,
+            create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+        )`,
+		`CREATE TABLE IF NOT EXISTS secret_data (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            type VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            metadata TEXT,
+            data BYTEA NOT NULL,
+            version INTEGER DEFAULT 1,
+            create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+            update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        )`,
+	}
+
+	for _, query := range queries {
+		_, err := db.Exec(query)
+		if err != nil {
+			return fmt.Errorf("failed to execute query: %w\nQuery: %s", err, query)
+		}
+	}
+	return nil
 }
